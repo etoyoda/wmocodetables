57f6ab34e5b6bcb563aec37d708168dcf0b10b28
TOYODA Eizi <toyoda.eizi@gmail.com>
2026-01-23 00:13:13 +0000

    create tagged directory if repository is already checked out

--- co.sh
+++ co.sh
@@ -3,17 +3,24 @@
 PATH=/bin:/usr/bin:/usr/local/bin
 
 dir=$(dirname $0)
+if [ X"${dir}" = X"." ]; then
+  dir=$(pwd)
+fi
 
 ftname='FT2026-1'
 
-for rep in GRIB2
+for rep in GRIB2 BUFR4 CCT
 do
   cd ${dir}
   if [ ! -d ${rep} ] ; then
     git clone https://github.com/wmo-im/${rep}
+    cd ${rep}
+    git fetch --all --prune
+    git checkout main
   fi
+  cd ${dir}
   cd ${rep}
-  git fetch --all --prune
-  git checkout main
-  git worktree add ../${rep}-${ftname} origin/${ftname}
+  if [ ! -d ../${rep}-${ftname} ]; then
+    git worktree add ../${rep}-${ftname} origin/${ftname}
+  fi
 done
