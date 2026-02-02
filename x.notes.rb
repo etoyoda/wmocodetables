bf47a33ef01fec9e40c37697a6e2d936002163e4
TOYODA Eizi <toyoda.eizi@gmail.com>
2026-02-02 14:40:14 +0000

    Note_en にあり得る文字列のパターンを確認する

--- x.notes.rb
+++ x.notes.rb
@@ -1,10 +1,15 @@
 require 'csv'
+
+PAT=%r{^ ?\((flags - )?([Ss]ee \t*)?(( and )?((Code|Flag) table \d\.(\d+|PTN)|Common Code table  ?C[-\u{2013}]\d+( in Part C\/c\.)?|Note|[Nn]otes? \d+(, \d+)*( and \d+)?))+\)? ?$}
+
 Dir.glob('{GRIB2,BUFR4,CCT}/*.csv').each{|cfnam|
   tab=CSV.read(cfnam,headers:true)
   next unless tab.headers.include?('Note_en')
   puts cfnam
   tab.each{|row|
     next unless row['Note_en']
-    puts row['Note_en'].inspect
+    text=row['Note_en']
+    raise text unless PAT===text
+    puts text.inspect
   }
 }
