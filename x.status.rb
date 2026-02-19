30622c96d9e79b1f3c58bc4e68d25fc2ccd54333
TOYODA Eizi <toyoda.eizi@gmail.com>
2026-02-19 00:22:01 +0000

    documentation

--- x.status.rb
+++ x.status.rb
@@ -1,10 +1,21 @@
 require 'csv'
+db=Hash.new
 Dir.glob('{GRIB2,BUFR4,CCT}/*.csv').each{|cfnam|
   tab=CSV.read(cfnam,headers:true)
   next unless tab.headers.include?('Status')
   puts cfnam
   tab.each{|row|
-    next if /^(Operational|Deprecated)$/===row['Status']
-    puts row.to_h.inspect
+    status=row['Status'].inspect
+    next if '"Operational"'==status
+    puts row.to_h.inspect unless '"Deprecated"'==status
+    db[status]=Hash.new unless db[status]
+    db[status][cfnam]=true
+  }
+}
+
+db.keys.sort.each{|status|
+  puts "== status #{status}"
+  db[status].keys.sort.each{|cfnam|
+    puts cfnam
   }
 }
