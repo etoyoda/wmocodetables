be6e5f4d05e427dcb772d78690661f3a5a6aed3a
TOYODA Eizi <toyoda.eizi@gmail.com>
2026-03-13 00:12:33 +0000

    Note 列も対象に追加

--- x.notes.rb
+++ x.notes.rb
@@ -9,8 +9,8 @@ Dir.glob('{GRIB2,BUFR4,CCT}/*.csv').each{|cfnam|
   next unless tab.headers.include?('Note_en')
   puts cfnam
   tab.each{|row|
-    next unless row['Note_en']
-    text=row['Note_en']
+    text=row['Note_en']||row['Note']
+    next unless text
     raise text unless PAT===text
     puts text.inspect
   }
